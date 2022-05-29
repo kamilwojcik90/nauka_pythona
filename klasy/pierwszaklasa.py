65a8c9271754ec047cad20fe4d866ffe9c3ddf8e
kamilwojcik90 <kamilwojcik90@users.noreply.github.com>
2022-05-29 13:00:46 +0200

    poprawka2

--- klasy/pierwszaklasa.py
+++ klasy/pierwszaklasa.py
@@ -3,13 +3,27 @@
 # pass moze byc uzyte w ciele metody ktore nioc nie robi
 
 # self reprezentuje instacje klasy, dzieki uzyciu self mozemy miec dostep do wlasciwosci i metod klasy w pythonie
+# Tworzenie obiektu realizujemy przez tzw. konstruktor - jest to specjalna metoda, ktora wykonywana kiedy tworzymy nasz obiekt
 class Paletka:
     def __init__(self, kolor):
+        self.kolor_obiektu = kolor
+        print(f"Utworzylismy obiekt o kolorze: {self.kolor_obiektu} - ID: {id(self)}")
     #pass
 
 # tworzymy obiekt na podstawie klasy
 def testklasy():
-    paletka_a = Paletka()
+    paletka_a = Paletka("czerwony")
+    paletka_b = Paletka("niebieski")
+    print("")
     print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
     # print("Obiekt typu " + {type(paletka_a)} + " zawiera od razu pewne właściwości i metody:")
     print(dir(paletka_a))
+
+    print("")
+    print(f"Obiekt typu {type(paletka_b)} zawiera od razu pewne właściwości i metody:")
+    # print("Obiekt typu " + {type(paletka_a)} + " zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_b))
+
+    print("")
+    print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
+    print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
