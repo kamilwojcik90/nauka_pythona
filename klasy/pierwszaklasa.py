524992bf7da782d1faaceef42f239804faa49316
kamilwojcik90 <kamilwojcik90@users.noreply.github.com>
2022-05-29 12:38:09 +0200

    poprawka

--- klasy/pierwszaklasa.py
+++ klasy/pierwszaklasa.py
@@ -1,11 +1,15 @@
 # najprostrzy sposob definiowania obiektu
 # pass jest to miejsce dla funkcjonalnosci, ktora bedzie dodana pozniej
 # pass moze byc uzyte w ciele metody ktore nioc nie robi
+
+# self reprezentuje instacje klasy, dzieki uzyciu self mozemy miec dostep do wlasciwosci i metod klasy w pythonie
 class Paletka:
-    pass
+    def __init__(self, kolor):
+    #pass
 
 # tworzymy obiekt na podstawie klasy
-paletka_a = Paletka()
-print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
-# print("Obiekt typu " + {type(paletka_a)} + " zawiera od razu pewne właściwości i metody:")
-print(dir(paletka_a))
+def testklasy():
+    paletka_a = Paletka()
+    print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
+    # print("Obiekt typu " + {type(paletka_a)} + " zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_a))
